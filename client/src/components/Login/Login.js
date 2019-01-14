58a58a40616ed3413040a8ed4705b41c0b812bc1
刘灿 <black.angel.liu@gmail.com>
2019-01-14 17:48:45 +0800

    提交学习版本。

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './login.css'
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="components-login">
+        <h3>Login</h3>
+        <div>
+          <button>提交</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Login;
