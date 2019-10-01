1566529c1265b1824e48cae6cd28fb39f7e7bc77
可乐加糖 <33370733@qq.com>
2019-10-01 23:43:24 +0800

    解决了消息列表的滚动和排列问题。

--- client/src/components/List/MsgList.js
+++ client/src/components/List/MsgList.js
@@ -1,40 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import ImageIcon from '@material-ui/icons/Image';
-import WorkIcon from '@material-ui/icons/Work';
-import BeachAccessIcon from '@material-ui/icons/BeachAccess';
 
-export default function SimpleContainer() {
+import "./MsgList.css";
+
+
+class MsgList extends Component{
+  render() {
+    let listItems = this.props.dataList;
+    console.log("listItems:",listItems);
+    return (
+      <List>
+        {listItems.map((item) => {
+          return (
+            <ListItem key={item.key}>
+              <ListItemAvatar>
+                <Avatar>
+                  <ImageIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText secondary={item.text} />
+            </ListItem>
+          )
+        })}
+      </List>
+    );
+  }
+}
+
+export default MsgList;
+
+/*export default function SimpleContainer() {
+  const classes = useStyles();
+  console.log("this.props:", this);
+  let listItems = []; 
+  
   return (
     <List>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <ImageIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <WorkIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Work" secondary="Jan 7, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <BeachAccessIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Vacation" secondary="July 20, 2014" />
-      </ListItem>
+      {listItems.map((item) => {
+        return (
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar className={classes.bigAvatar}>
+                <ImageIcon />
+              </Avatar>
+              <span className="nick-name">昵称</span>
+            </ListItemAvatar>
+            <ListItemText secondary={item.text} />
+          </ListItem>
+        )
+      })}
     </List>
   )
 }
+*/
--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,63 +1,87 @@
-import React from 'react';
-
-import './Main.css';
-
+import React, { Component } from 'react';
 import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-
 import MsgList from "../List/MsgList.js";
 
+import './Main.css';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+class Main extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      listItems:[
+        { key: '0', value: 'Photos', text: '就是可怜的肌肤阿拉山口将大幅拉升的' },
+        { key: '1', value: 'Work', text: '就是可怜的肌肤阿拉山口将大幅拉升的' },
+        { key: '2', value: 'Vacation', text: '阿克琉斯记得反馈啦技术的开发李建阿斯利康的肌肤脸上看对方' },
+        { key: '3', value: 'Tokyo', text: '阿克苏的肌肤卢卡斯就打开了飞机阿斯利康的肌肤卢卡斯觉得浪费空间上来的咖啡机' },
+        { key: '4', value: 'HK', text: '看见了卡技术的开发 i 欧文哦日 u 为儿女们，苏丹诺夫' },
+        { key: '5', value: 'Photos', text: '就是可怜的肌肤阿拉山口将大幅拉升的' },
+        { key: '6', value: 'Work', text: '就是可怜的肌肤阿拉山口将大幅拉升的' },
+        { key: '7', value: 'Vacation', text: '阿克琉斯记得反馈啦技术的开发李建阿斯利康的肌肤脸上看对方' }
+      ]
+    }
+  }
+  enterText (event) {
+    const val = event.target.value;
+    const keyCode = event.which;
+    
+    if (keyCode === 13) {
+      let newArray = this.state.listItems;
+      let scrollHeight = document.querySelector(".contain-list").scrollHeight;
+      let lastChild = document.querySelector(".MuiListItem-root:last-child");
 
-export default function SimpleContainer() {
-  const classes = useStyles();
-  return (
-    <React.Fragment>
-      <Container fixed className="main-container">
+      newArray.push({
+        key: new Date().getTime(), 
+        value: new Date().getTime(), 
+        text: val
+      });
+      this.setState({listItems: newArray});
+      event.target.value = "";
+      setTimeout(() => {
+        document.querySelector(".contain-list").scrollTop = 9999999;
+      }, 500);
+    }
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <Container fixed className="main-container">
           <AppBar position="static" className="app-title">
             <Toolbar>
-              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <IconButton edge="start" color="inherit" aria-label="menu">
                 <MenuIcon />
               </IconButton>
-              <Typography variant="h6" className={classes.title}>
+              <Typography variant="h6">
                 Hello 楽
               </Typography>
             </Toolbar>
           </AppBar>
           {/* 内容窗口 */}
           <div className="contain-list">
-            <MsgList></MsgList>
+            <MsgList dataList={this.state.listItems}></MsgList>
           </div>
           {/* 底部输入框 */}
           <div className="footer">
             <TextField
               fullWidth
               id="outlined-bare"
-              className={classes.textField}
               placeholder="请输入您的内容"
               margin="normal"
               variant="outlined"
+              onKeyPress={(e) => {
+                this.enterText(e);
+              }}
             />
           </div>
-      </Container>
-    </React.Fragment>
-  );
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
+
+export default Main;
